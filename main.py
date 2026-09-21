ad599e1905284e1fc97c331b5f685d5448e66533
rehanalisha777-nar <rehanalisha777@gmail.com>
2026-09-21 21:43:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+print("==============================")
+print("       ELITE TRACKER")
+print("==============================")
 
+name = input("Enter your name: ")
+
+xp = 0
+level = 1
+streak = 0
+
+print()
+print("Welcome,", name)
+print("Level:", level)
+print("XP:", xp)
+print("🔥 Streak:", streak, "days")
